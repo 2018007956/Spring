c7acd9ba042c72114d7abf319351180ee8099ff0
2018007956 <bbmt1906@hanyang.ac.kr>
2025-02-27 15:17:16 +0900

    feat: member service

--- spring-start/src/test/java/study/spring_start/service/MemberServiceTest.java
+++ spring-start/src/test/java/study/spring_start/service/MemberServiceTest.java
@@ -1,4 +1,58 @@
-import static org.junit.jupiter.api.Assertions.*;
+package study.spring_start.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import study.spring_start.domain.Member;
+import study.spring_start.repository.MemoryMemberRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class MemberServiceTest {
-  
+
+    MemoryMemberRepository memberRepository;
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository); // Dependency Injection
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); // 오른쪽 로직을 실행하면 왼쪽 예외가 터져야 함
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
 }
